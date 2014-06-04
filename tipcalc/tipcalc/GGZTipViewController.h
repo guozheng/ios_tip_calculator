a353b34cee3f81b368a876a0346bd8517bd47ada
Guozheng Ge <gzge@yahoo-inc.com>
2014-06-04 10:23:14 -0700

    UI part done

---
+++ tipcalc/tipcalc/GGZTipViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGZTipViewController.h
+//  tipcalc
+//
+//  Created by Guozheng Ge on 6/4/14.
+//  Copyright (c) 2014 gzge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGZTipViewController : UIViewController
+
+@end
