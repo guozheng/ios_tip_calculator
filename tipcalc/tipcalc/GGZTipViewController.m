48edc406702e0027a87fec2c0cdace062c7d34fb
Guozheng Ge <gzge@yahoo-inc.com>
2014-06-04 23:21:00 -0700

    finish up the logic, add screencast

--- tipcalc/tipcalc/GGZTipViewController.m
+++ tipcalc/tipcalc/GGZTipViewController.m
@@ -10,6 +10,14 @@
 
 @interface GGZTipViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
+
 @end
 
 @implementation GGZTipViewController
@@ -28,12 +36,45 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self updateValues];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    double billAmount = [self.billTextField.text doubleValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2)];
+    double tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] doubleValue];
+    double totalAmount = billAmount + tipAmount;
+    
+    NSLog(@"billAmount=%.2f, tipAmount=%.2f, totalAmount=%.2f", billAmount, tipAmount, totalAmount);
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", totalAmount];
+}
+
+// actions to happen when pressing enter in the billTextField
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self.billTextField resignFirstResponder];
+    [self.view endEditing:YES];
+    [self updateValues];
+    return YES;
+}
+
+// clear tip and total labels
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    self.tipLabel.text = @"$0.00";
+    self.totalLabel.text = @"$0.00";
+    return YES;
 }
 
 @end
